efcdc2e20c4155f90892c3317e3cd07973ca8135
ashkankhan786 <ashkankhan728@gmail.com>
2025-05-20 21:31:42 +0530

    updated the port in app.py

--- app.py
+++ app.py
@@ -86,4 +86,5 @@ def predict():
     return jsonify({'prediction': result})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
